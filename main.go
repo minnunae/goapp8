23d436a33fdd567524b896070644b2ca852c0d90
minnunae <69183312+minnunae@users.noreply.github.com>
2021-12-25 15:05:51 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/patipan-patisampita/goapp8/functions"
+)
+
+func main() {
+	// fmt.Println("Hello World!")
+	// fmt.Println(quote.HelloV3())
+	// variables.Learn()
+	functions.Learn()
+}
